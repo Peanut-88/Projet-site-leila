c0d5409f7aa9a54b28b95765b13b9d0bf0192211
Rachid <ahmed-rachid@outlook.com>
2020-11-11 19:34:46 +0100

    Starting home page

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,6 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+import StickyHeader from './modules/StickyHeader'
+
+new MobileMenu()
+new StickyHeader()
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,25 @@
+import throttle from 'lodash/throttle'
+
+class StickyHeader {
+    constructor() {
+        this.siteHeader = document.querySelector(".site-header")
+        this.events()
+    }
+
+    events() {
+        window.addEventListener("scroll", throttle(() => this.runOnScroll(), 200))
+        
+    }
+
+    runOnScroll() {
+        if(window.scrollY > 100) {
+            this.siteHeader.classList.add("site-header--sticky")
+        } else {
+            this.siteHeader.classList.remove("site-header--sticky")
+        }
+        
+    }
+}
+
+
+export default StickyHeader
