667c91a19405733151f315031002a9b82bba31e0
Rachid <ahmed-rachid@outlook.com>
2020-11-12 20:40:50 +0100

    Header modification

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,7 +1,6 @@
 class MobileMenu {
     constructor () {
         this.menuIcon = document.querySelector(".site-header__menu-icon")
-        this.menuContent = document.querySelector(".site-header__menu-content")
         this.siteHeader =  document.querySelector(".site-header")
 
         this.events();
@@ -13,7 +12,7 @@ class MobileMenu {
 
     toggleTheMenu() {
         this.menuIcon.classList.toggle("site-header__menu-icon--close-x")
-        this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+        this.siteHeader.classList.toggle("site-header--is-visible")
     }
 }
 
